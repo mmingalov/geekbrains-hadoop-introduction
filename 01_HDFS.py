1b5a593bae99db9980a359fb3c599ce5ce9a593e
mmingalov <mmingalov@gmail.com>
2020-03-16 16:23:23 +1000

    01

---
+++ 01_HDFS.py
@@ -0,0 +1,11 @@
+from hdfs3 import HDFileSystem
+hdfss = HDFileSystem(host='89.208.221.132', port=7180)
+hdfss.ls('/')
+#hdfs.put('local-file.txt', '/user/data/remote-file.txt')
+#hdfs.cp('/user/data/file.txt', '/user2/data')
+
+# 89.208.221.132 manager.novalocal
+# 89.208.222.81 node1.novalocal
+# 89.208.220.216 node2.novalocal
+# 89.208.222.201 node3.novalocal
+# https://stackoverflow.com/questions/43987081/openslide-python-import-error
